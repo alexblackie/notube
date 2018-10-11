77989858ab93a721f5064876c0a0ef6188ab7841
Alex Blackie <alex@alexblackie.com>
2018-10-11 15:12:52 -0400

    Add sinatra app and home page
    This begins adding stuff for the web UI component of this project. So
    far, a very basic sinatra skeleton, some basic CSS, and a "latest 10"
    home page.

---
+++ lib/notube.rb
@@ -0,0 +1,5 @@
+require "sinatra"
+require "sqlite3"
+require "pry" if development?
+
+require "notube/application"
---
+++ lib/notube/application.rb
@@ -0,0 +1,22 @@
+module Notube
+  class Application < Sinatra::Base
+
+    set :db, SQLite3::Database.new("library.db")
+    set :public_folder, Proc.new { File.join(root, "..", "..", "static") }
+
+    get "/" do
+      @channels = settings.db.execute("select id,external_id,name from channels")
+
+      columns = [
+        "videos.id", "videos.external_id", "videos.title", "channels.id",
+        "channels.external_id", "channels.name"
+      ].join(",")
+      @videos = settings.db.execute("select #{ columns } from videos " +
+                  "inner join channels on videos.channel_id = channels.id " +
+                  "order by videos.created_at desc limit 10")
+
+      erb :home
+    end
+
+  end
+end
